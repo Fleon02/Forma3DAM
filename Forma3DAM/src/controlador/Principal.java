edc9fd07caa742d19e543a5bce59fa43f70c83c6
Fleon02 <leonmarcosf@gmail.com>
2024-02-08 17:51:59 +0100

    Primer Conmit

---
+++ Forma3DAM/src/controlador/Principal.java
@@ -0,0 +1,13 @@
+package controlador;
+
+import vista.VntPrincipal;
+
+public class Principal {
+    public static void main(String[] args) {
+        // Crear una instancia de VntPrincipal
+        VntPrincipal ventanaPrincipal = new VntPrincipal();
+        
+        // Hacer visible la ventana
+        ventanaPrincipal.setVisible(true);
+    }
+}
