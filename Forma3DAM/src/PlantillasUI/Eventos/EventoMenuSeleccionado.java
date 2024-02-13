acdd204b792882aed498e0ee95e1c6f4803f48f4
DavidCorredorMiguel <davidcorredormiguel25599@gmail.com>
2024-02-13 17:44:59 +0100

    e

--- Forma3DAM/src/PlantillasUI/Eventos/EventoMenuSeleccionado.java
+++
@@ -1,9 +0,0 @@
-
-package PlantillasUI.Eventos;
-
-public class EventoMenuSeleccionado {
-
-    public void menuSeleccionado(int indexMenu, int indexSubMenu){
-        
-    };
-}
--- Forma3DAM/src/PlantillasUI/SystemTheme.java
+++
@@ -1,9 +0,0 @@
-package PlantillasUI;
-
-import java.awt.Color;
-
-public class SystemTheme {
-
-    //  Default theme color
-    public static Color mainColor = new Color(54, 149, 255);
-}
