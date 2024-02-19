7ea76d8ed0fe215e4054cff63bc020da9c171537
DavidCorredorMiguel <davidcorredormiguel25599@gmail.com>
2024-02-19 17:52:01 +0100

    a

--- Forma3DAM/src/vista/VntAlumnos.java
+++ Forma3DAM/src/vista/VntAlumnos.java
@@ -68,11 +68,12 @@ public class VntAlumnos extends javax.swing.JPanel {
                 if (!e.getValueIsAdjusting()) {
                     int filas = TablaAlumnos.getSelectedRow();
                     if (filas != -1) {
-                        txtDNIAlumno.setText(TablaAlumnos.getValueAt(filas, 0) + "");
-                        txtNombreAlumno.setText(TablaAlumnos.getValueAt(filas, 1) + "");
-                        txtAnioAlumno.setText(TablaAlumnos.getValueAt(filas, 2) + "");
-                        txtNSSAlumno.setText(TablaAlumnos.getValueAt(filas, 3) + "");
-                        txtCicloAlumno.setText(TablaAlumnos.getValueAt(filas, 4) + "");
+                        txtIDAlumno.setText(TablaAlumnos.getValueAt(filas, 0) + "");
+                        txtDNIAlumno.setText(TablaAlumnos.getValueAt(filas, 1) + "");
+                        txtNombreAlumno.setText(TablaAlumnos.getValueAt(filas, 2) + "");
+                        txtAnioAlumno.setText(TablaAlumnos.getValueAt(filas, 3) + "");
+                        txtNSSAlumno.setText(TablaAlumnos.getValueAt(filas, 4) + "");
+                        txtCicloAlumno.setText(TablaAlumnos.getValueAt(filas, 5) + "");
                         if (TablaAlumnos.getValueAt(filas, 6).equals(true)) {
                             checkbValidez.setSelected(true);
                         } else {
@@ -81,6 +82,11 @@ public class VntAlumnos extends javax.swing.JPanel {
                         btnSubirCV.setEnabled(true);
                         btnActualizar.setEnabled(true);
                         btnBorrar.setEnabled(true);
+                        txtDNIAlumno.setEditable(true);
+                        txtNombreAlumno.setEditable(true);
+                        txtAnioAlumno.setEditable(true);
+                        txtNSSAlumno.setEditable(true);
+                        txtCicloAlumno.setEditable(true);
                     } else {
                         txtDNIAlumno.setEditable(false);
                         txtNombreAlumno.setEditable(false);
@@ -119,6 +125,8 @@ public class VntAlumnos extends javax.swing.JPanel {
         btnActualizar = new javax.swing.JButton();
         txtCicloAlumno = new javax.swing.JTextField();
         title1 = new javax.swing.JLabel();
+        txtIDAlumno = new javax.swing.JTextField();
+        userLabel6 = new javax.swing.JLabel();
 
         jTableAlumnos.setModel(dtm);
         jScrollPane1.setViewportView(jTableAlumnos);
@@ -127,12 +135,10 @@ public class VntAlumnos extends javax.swing.JPanel {
         favicon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/logoF.png"))); // NOI18N
 
         title.setFont(new java.awt.Font("Roboto Black", 1, 24)); // NOI18N
-        title.setForeground(new java.awt.Color(0, 0, 0));
         title.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         title.setText("Actualizar/Borrar");
 
         userLabel.setFont(new java.awt.Font("Roboto Light", 1, 14)); // NOI18N
-        userLabel.setForeground(new java.awt.Color(0, 0, 0));
         userLabel.setText("DNI Alumno");
 
         txtDNIAlumno.setEditable(false);
@@ -150,7 +156,6 @@ public class VntAlumnos extends javax.swing.JPanel {
         jScrollPane2.setViewportView(TablaAlumnos);
 
         userLabel1.setFont(new java.awt.Font("Roboto Light", 1, 14)); // NOI18N
-        userLabel1.setForeground(new java.awt.Color(0, 0, 0));
         userLabel1.setText("Nombre Alumno");
 
         txtNombreAlumno.setEditable(false);
@@ -165,7 +170,6 @@ public class VntAlumnos extends javax.swing.JPanel {
         });
 
         userLabel2.setFont(new java.awt.Font("Roboto Light", 1, 14)); // NOI18N
-        userLabel2.setForeground(new java.awt.Color(0, 0, 0));
         userLabel2.setText("Año Alumno");
 
         txtAnioAlumno.setEditable(false);
@@ -180,7 +184,6 @@ public class VntAlumnos extends javax.swing.JPanel {
         });
 
         userLabel3.setFont(new java.awt.Font("Roboto Light", 1, 14)); // NOI18N
-        userLabel3.setForeground(new java.awt.Color(0, 0, 0));
         userLabel3.setText("N. S. S. Alumno");
 
         txtNSSAlumno.setEditable(false);
@@ -195,11 +198,9 @@ public class VntAlumnos extends javax.swing.JPanel {
         });
 
         userLabel4.setFont(new java.awt.Font("Roboto Light", 1, 14)); // NOI18N
-        userLabel4.setForeground(new java.awt.Color(0, 0, 0));
         userLabel4.setText("Ciclo Alumno");
 
         userLabel5.setFont(new java.awt.Font("Roboto Light", 1, 14)); // NOI18N
-        userLabel5.setForeground(new java.awt.Color(0, 0, 0));
         userLabel5.setText("Curriculum Alumno");
 
         btnBorrar.setBackground(new java.awt.Color(18, 30, 49));
@@ -214,7 +215,6 @@ public class VntAlumnos extends javax.swing.JPanel {
         });
 
         checkbValidez.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        checkbValidez.setForeground(new java.awt.Color(0, 0, 0));
         checkbValidez.setText("Validez (Aprobado)");
 
         btnSubirCV.setBackground(new java.awt.Color(18, 30, 49));
@@ -251,15 +251,36 @@ public class VntAlumnos extends javax.swing.JPanel {
         });
 
         title1.setFont(new java.awt.Font("Roboto Black", 1, 24)); // NOI18N
-        title1.setForeground(new java.awt.Color(0, 0, 0));
         title1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         title1.setText("ALUMNOS");
+
+        txtIDAlumno.setEditable(false);
+        txtIDAlumno.setBackground(new java.awt.Color(0, 0, 0));
+        txtIDAlumno.setFont(new java.awt.Font("Roboto", 0, 12)); // NOI18N
+        txtIDAlumno.setForeground(new java.awt.Color(255, 255, 255));
+        txtIDAlumno.setBorder(null);
+        txtIDAlumno.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                txtIDAlumnoMousePressed(evt);
+            }
+        });
+
+        userLabel6.setFont(new java.awt.Font("Roboto Light", 1, 14)); // NOI18N
+        userLabel6.setText("ID Alumno");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1090, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(684, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(title, javax.swing.GroupLayout.PREFERRED_SIZE, 370, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(userLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, 0)
+                        .addComponent(txtIDAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(26, 26, 26))
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
                     .addGap(0, 25, Short.MAX_VALUE)
@@ -268,9 +289,7 @@ public class VntAlumnos extends javax.swing.JPanel {
                             .addGap(40, 40, 40)
                             .addComponent(favicon, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGap(80, 80, 80)
-                            .addComponent(title1, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGap(220, 220, 220)
-                            .addComponent(title, javax.swing.GroupLayout.PREFERRED_SIZE, 370, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(title1, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGroup(layout.createSequentialGroup()
                             .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 640, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGap(20, 20, 20)
@@ -311,7 +330,14 @@ public class VntAlumnos extends javax.swing.JPanel {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 608, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(title, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(userLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtIDAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(503, Short.MAX_VALUE))
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
                     .addGap(0, 0, Short.MAX_VALUE)
@@ -319,8 +345,7 @@ public class VntAlumnos extends javax.swing.JPanel {
                         .addComponent(favicon, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGroup(layout.createSequentialGroup()
                             .addGap(10, 10, 10)
-                            .addComponent(title1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addComponent(title, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(title1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGroup(layout.createSequentialGroup()
@@ -386,10 +411,10 @@ public class VntAlumnos extends javax.swing.JPanel {
         if (txtDNIAlumno.getText() != "" && txtNombreAlumno.getText() != "" && txtAnioAlumno.getText() != ""
                 && txtNSSAlumno.getText() != "" && txtCicloAlumno.getText() != "") {
             if (checkbValidez.isSelected()) {
-                new AlumnosDAO().actualizaAlumnos(txtDNIAlumno.getText(), txtNombreAlumno.getText(), txtAnioAlumno.getText(),
+                new AlumnosDAO().actualizaAlumnos(Integer.parseInt(txtIDAlumno.getText()), txtDNIAlumno.getText(), txtNombreAlumno.getText(), txtAnioAlumno.getText(),
                         txtNSSAlumno.getText(), 1, txtCicloAlumno.getText(), bytesCV);
             } else {
-                new AlumnosDAO().actualizaAlumnos(txtDNIAlumno.getText(), txtNombreAlumno.getText(), txtAnioAlumno.getText(),
+                new AlumnosDAO().actualizaAlumnos(Integer.parseInt(txtIDAlumno.getText()), txtDNIAlumno.getText(), txtNombreAlumno.getText(), txtAnioAlumno.getText(),
                         txtNSSAlumno.getText(), 0, txtCicloAlumno.getText(), bytesCV);
             }
             cargaTabla();
@@ -417,6 +442,10 @@ public class VntAlumnos extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_btnSubirCVActionPerformed
 
+    private void txtIDAlumnoMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtIDAlumnoMousePressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtIDAlumnoMousePressed
+
     private byte[] convertirArchivoABytes(File archivo) throws IOException {
         byte[] bytesArray = new byte[(int) archivo.length()];
         FileInputStream fis = new FileInputStream(archivo);
@@ -439,6 +468,7 @@ public class VntAlumnos extends javax.swing.JPanel {
     private javax.swing.JTextField txtAnioAlumno;
     private javax.swing.JTextField txtCicloAlumno;
     private javax.swing.JTextField txtDNIAlumno;
+    private javax.swing.JTextField txtIDAlumno;
     private javax.swing.JTextField txtNSSAlumno;
     private javax.swing.JTextField txtNombreAlumno;
     private javax.swing.JLabel userLabel;
@@ -447,5 +477,6 @@ public class VntAlumnos extends javax.swing.JPanel {
     private javax.swing.JLabel userLabel3;
     private javax.swing.JLabel userLabel4;
     private javax.swing.JLabel userLabel5;
+    private javax.swing.JLabel userLabel6;
     // End of variables declaration//GEN-END:variables
 }
