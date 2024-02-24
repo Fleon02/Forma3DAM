159d5d77368f98402977b257ac6a60c275642c2b
zzzwaspzzz <98188723+zzzwaspzzz@users.noreply.github.com>
2024-02-24 22:00:40 +0100

    Necesidad - Creada Ventana y ClaseDAO

---
+++ Forma3DAM/src/modelo/NecesidadDAO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import controlador.HibernateUtil;
+import java.awt.Component;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NecesidadDAO {
+    private Session sesion;
+    private Transaction tx;
+    Component parentComponent = null;
+
+    private void iniciaOperacion() {
+        Logger.getLogger("org.hibernate").setLevel(Level.OFF);
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        tx = sesion.beginTransaction();
+    }
+
+    private void manejaExcepcion(HibernateException he) {
+        tx.rollback();
+        throw new HibernateException("Error en la capa de acceso a datos", he);
+    }
+    
+    
+}
